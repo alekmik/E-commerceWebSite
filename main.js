3d8fe4d1ab642cee9dcdc2ad0f7f8b142ec91dfd
Alek Mikolajek <alekmikopro@gmail.com>
2022-01-18 09:35:04 +0100

    second jet

---
+++ main.js
@@ -0,0 +1,5 @@
+/* connexion des boutons "ajouter au panier" avec une variable JS*/
+let AddToCart = document.getElementsByClassName('addToCart');
+console.log(AddToCart);
+
+/* creation de la fonction */
